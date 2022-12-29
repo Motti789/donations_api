50be6ab7bee37cfb44310eb9d2ff55677d2250ad
Motti789 <mchillywilly@gmail.com>
2022-12-29 11:15:40 -0500

    added the realationship and validations to the models

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,5 @@
 class Donation < ApplicationRecord
+    belongs_to :donor
+
+    validates :amount, presence: true
 end
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,2 +1,5 @@
 class Donor < ApplicationRecord
+    has_many :donations
+
+    validates :first_name, :last_name, :email, :phone_number, presence: true
 end
