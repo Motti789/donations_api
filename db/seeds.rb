7bade07d98f702fdea4ad35f735c0876353d2ee4
Motti789 <mchillywilly@gmail.com>
2023-01-02 11:39:07 -0500

    seeded the database with dummy data to make sure everything is working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Donation.destroy_all
-Donor.destroy_all
+# Donation.destroy_all
+# Donor.destroy_all
 
 
+donor1 = Donor.create(first_name: "Motti", last_name: "Wilschanski", email: "Mchillywilly@gmail.com", phone_number: "718-885-5694")
+donor2 = Donor.create(first_name: "Yitty", last_name: "Rendler", email: "Yitrendler@gmail.com", phone_number: "718-864-5273")
+donor3 = Donor.create(first_name: "Shmuli", last_name: "Wilschanski", email: "Samtheman@gmail.com", phone_number: "718-565-2619")
 
 
+donation1 = Donation.create(amount: 12.50, donor_id: 1)
+donation2 = Donation.create(amount: 10, donor_id: 2)
+donation3 = Donation.create(amount: 18.75, donor_id: 3)
 
-# suv = Category.create(name: "SUV")
-# electric = Category.create(name: "Electric")
-# van = Category.create(name: "Van")
-
-# car1 = Car.create(brand: "Nissan", model: "SV", exterior_color: "Silver", interior_color: "Black", fuel_type: "gas", category_id: 1)
-# car2 = Car.create(brand: "Tesla", model: "S", exterior_color: "Red", interior_color: "White", fuel_type: "Non", category_id: 2)
-# car3 = Car.create(brand: "Honda", model: "Odyssey", exterior_color: "Green", interior_color: "Black", fuel_type: "gas", category_id: 3)
